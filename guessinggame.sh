d2c8564724f768dfa546f2decb1e4cd0f81c61b7
Dikra Guiri <144810360+DiKraGu@users.noreply.github.com>
2024-12-11 21:28:43 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,30 @@
 
+#!/bin/bash
+
+# Function to count files in the current directory
+function count_files {
+    ls | wc -l
+}
+
+# Main function
+function guessing_game {
+    local guess
+    local correct_answer=$(count_files)
+    
+    while true; do
+        echo "Guess the number of files in the current directory:"
+        read -r guess
+        
+        if [[ $guess -eq $correct_answer ]]; then
+            echo "Congratulations! You guessed it right."
+            break
+        elif [[ $guess -lt $correct_answer ]]; then
+            echo "Too low! Try again."
+        else
+            echo "Too high! Try again."
+        fi
+    done
+}
+
+# Execute the guessing game
+guessing_game
